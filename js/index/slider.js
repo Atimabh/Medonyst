581bc1fb0450c0c2f97c9b6413b0e35ac366e185
atimabh <abarunaha@gmail.com>
2020-12-01 00:00:29 +0530

    alert

--- js/index/slider.js
+++ js/index/slider.js
@@ -1,3 +1,6 @@
+window.onload = (event) => {
+    alert('This page is best viewed on a mobile device')
+};
 var swiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
